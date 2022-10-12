6f00dbece8192237d99e69d502f7b9b5335cd68b
gabriel2170 <64446258+gabriel2170@users.noreply.github.com>
2022-10-12 14:09:16 -0300

    up

---
+++ Curso React + Redux COD3R/fundamentos-react/src/components/basicos/Primeiro.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Primeiro(){
+
+    return <div>
+        <h2>
+            Meu Primeiro Componente
+        </h2>
+    </div>
+}
---
+++ Curso React + Redux COD3R/fundamentos-react/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Primeiro from './components/basicos/Primeiro'
+import './index.css'
+
+const tag = <h1>Hello</h1>
+
+ReactDOM.render(
+  <div>
+    <Primeiro></Primeiro>
+
+  </div>
+  ,
+  document.getElementById('root')
+
+
+)
